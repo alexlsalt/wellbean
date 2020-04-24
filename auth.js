606cb872d3254d399d5f14fcdbeba22430ccd0c1
Alex Morton <am@alexmortoncreative.com>
2020-04-24 17:09:11 +0200

    Initial commit...finally!

---
+++ auth.js
@@ -0,0 +1,98 @@
+// get data
+
+const currentAlertsButton = document.querySelector('#account__btn');
+
+// refs for main section based on login/out
+const loggedIn = document.querySelectorAll('.logged-in');
+const loggedOut = document.querySelectorAll('.logged-out');
+
+// tracking state changes based on logged in and logged in states
+auth.onAuthStateChanged(user => {
+  if(user) {
+    console.log('user logged in');
+  
+    loggedIn.forEach(el => {
+      el.style.display = 'block';
+    });
+
+    loggedOut.forEach(el => {
+      el.style.display = 'none';
+    });
+  } else {
+    console.log('user logged out');
+    loggedIn.forEach(el => {
+      el.style.display = 'none';
+    });
+
+    loggedOut.forEach(el => {
+      el.style.display = 'block';
+    });
+  }
+});
+
+
+///////////////// SIGNUP
+
+// get reference to signup form
+const signupForm = document.querySelector('#signup-form');
+
+// Add event listener to submit form
+signupForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+  auth.createUserWithEmailAndPassword(email, password)
+  .then(cred => {
+    // Form disappears
+    document.querySelector('.modal.signup').style.display = 'none';
+
+    // Clear input fields
+    signupForm.reset();
+  });
+
+    // Background opacity back to 1
+    document.querySelector('#content').style.opacity = 1;
+});
+
+
+///////////////// LOGIN
+
+// get reference to login form
+const loginForm = document.querySelector('#login-form');
+
+// event listener to login submit
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  auth.signInWithEmailAndPassword(email, password)
+  .then(cred => {
+
+  // Form disappears
+  document.querySelector('.modal.login').style.display = 'none';
+
+  // Clear input fields
+  loginForm.reset();
+  });
+
+  // Background opacity back to 1
+  document.querySelector('#content').style.opacity = 1;
+
+});
+
+
+///////////////// LOGOUT
+
+const logout = document.querySelector('#logout__btn');
+
+logout.addEventListener('click', event => {
+  event.preventDefault();
+
+  // sign out method using auth object
+  auth.signOut();
+
+});
---
+++ script.js
@@ -0,0 +1,187 @@
+const loginButton = document.querySelector('#login__btn');
+const signupButton = document.querySelector('#signup__btn');
+
+
+//////////////// LOGIN
+const loginModalAppear = () => {
+  document.querySelector('.login').style.display = 'block';
+  document.querySelector('#content').style.opacity = 0.6;
+  
+  // exit modal functionality
+  const exitLoginButton = document.querySelector('.exit.login');
+  const exitModal = () => {
+    document.querySelector('.modal.login').style.display = 'none';
+    document.querySelector('#content').style.opacity = 1;
+  };
+  exitLoginButton.addEventListener('click', exitModal);
+}
+
+loginButton.addEventListener('click', loginModalAppear);
+
+//////////////// SIGNUP
+const signupModalAppear = () => {
+  document.querySelector('.signup').style.display = 'block';
+  document.querySelector('#content').style.opacity = 0.6;
+
+  // exit modal functionality
+  const exitSignupButton = document.querySelector('.exit.signup');
+  const exitModal = () => {
+    document.querySelector('.modal.signup').style.display = 'none';
+    document.querySelector('#content').style.opacity = 1;
+    signupForm.reset();
+    
+  };
+
+  exitSignupButton.addEventListener('click', exitModal);
+}
+
+signupButton.addEventListener('click', signupModalAppear);
+
+
+
+    ///////////////////////////////
+
+    //////////////// ADDING CONTACT TO LIST 
+
+// get refs from UI input
+
+const contactForm = document.querySelector('#new-contact-form');
+
+contactForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const contactName = document.querySelector('#newContact__input').value;
+  const contactFrequency = document.querySelector('#newFrequency__input').value;
+
+  const ID = auth.currentUser.uid;
+
+  let data = {}
+
+  if (contactName !== '' && contactFrequency !== '') {
+    data['name'] = contactName;
+    data['frequency'] = contactFrequency;
+    data['id'] = ID;
+
+    db.collection('contacts').doc().set(data).then(() => {
+      console.log('Entered into database');
+    });
+
+    // Clear input fields
+    contactForm.reset();
+  } else {
+    alert('Please provide both a name and an alert frequency.');
+  }
+
+
+});
+
+
+// create element and render new contact on list
+
+const contactList = document.querySelector('.contact-list');
+
+function renderContact(doc) {
+  let li = document.createElement('li');
+  let name = document.createElement('span');
+  let frequency = document.createElement('span');
+
+
+  // Add in edit functionality at a later date (add to div HTML below) -->
+  // <i class="fas fa-edit"></i>
+
+  let div = '<div class="edit-exit-icons"></i><i class="fas fa-times x contact-delete"></i></div>'
+
+  li.setAttribute('data-id', doc.id);
+
+  name.textContent = doc.data().name;
+  frequency.textContent = doc.data().frequency;
+
+  li.appendChild(name);
+  li.appendChild(frequency);
+
+  li.insertAdjacentHTML('beforeend', div);
+  contactList.appendChild(li);
+
+  //////////////// DELETING CONTACTS FROM LIST
+
+  // 1. add event listener to delete button on list item (must be inside function because technically these li's don't exist yet on DOM)
+
+  document.querySelectorAll('.contact-delete').forEach(button => {
+    button.addEventListener('click', (event) => {
+      event.stopPropagation();
+
+      // 2. event target/bubbling to grab the list item id 
+      let deletedListItem = event.target.parentNode.parentNode;
+      let id = deletedListItem.getAttribute('data-id');
+  
+      // 3. delete the selected item from the parentNode (ul)
+      deletedListItem.parentNode.removeChild(deletedListItem);
+  
+      // 4. delete the item from the database
+      db.collection('contacts').doc(id).delete();
+    })
+  })
+
+}
+
+
+// THIS MATCHES UP CORRECT DOCS WITH USERS
+auth.onAuthStateChanged(user => {
+  if(user) {
+    // db.collection('contacts').get().then(snapshot => {
+    //   snapshot.docs.forEach(doc => {
+    //     if(doc.data().id === auth.currentUser.uid) {
+    //       console.log(doc.data().name);
+    //       renderContact(doc);
+    //     }
+    //   })
+    // })
+
+    // real-time listener
+    db.collection('contacts').onSnapshot(snapshot => {
+      let changes = snapshot.docChanges();
+      changes.forEach(change => {
+        if (change.type === 'added' && change.doc.data().id === auth.currentUser.uid) {
+          renderContact(change.doc);
+        } 
+      })
+    })
+  } else {
+    return;
+  }
+});
+
+// RESET CONTACT LIST ON LOGOUT
+
+const logoutButton = document.querySelector('#logout__btn');
+
+logoutButton.addEventListener('click', event => {
+  event.preventDefault();
+
+  contactList.innerHTML = '';
+})
+///////////////////////////////
+
+
+
+
+
+
+//////////////// EDITING CONTACT AND/OR FREQUENCY
+
+// 1. Add event listener to edit icon on list item
+
+// 2. Pop up modal with form and pre-filled values for selected ID 
+
+// 3. Add a 'save changes' button with event listener
+
+// 4. on Submit, prevent default and update UI and database
+
+
+
+// const documentRef = db.collection('contacts');
+
+//   documentRef.get().then(snapshot => {
+//     const documentID = snapshot.docs[0].id;
+//     console.log(documentID);
+//   });
